ab90ea1af64619d1124b546adbfa9ccf444e0fd6
weekdaycare <xiabee@duck.com>
2024-03-04 21:00:30 +0800

    🎉Update my site

--- js/services/twikoo_new.js
+++ js/services/twikoo_new.js
@@ -1,34 +1,30 @@
 utils.jq(() => {
     // 加载 Twikoo 的 JavaScript 脚本
     const twikooScript = document.createElement('script');
-    twikooScript.src = 'https://npm.onmicrosoft.cn/twikoo@latest';
+    twikooScript.src = 'https://npm.onmicrosoft.cn/twikoo@1.6.31';
     twikooScript.async = true;
     document.head.appendChild(twikooScript);
 
     twikooScript.onload = function() {
         // 当 Twikoo 脚本加载完成后执行加载评论的函数
         $(function () {
-            const els = document.getElementsByClassName('ds-comment');
+            const els = document.getElementsByClassName('ds-commentnew');
             for (var i = 0; i < els.length; i++) {
                 const el = els[i];
                 const api = el.getAttribute('api');
+                const limit = parseInt(el.getAttribute('limit')) || 10;
                 if (api == null) {
                     continue;
                 }
-                const default_avatar = def.avatar;
                 twikoo.getRecentComments({
-                    envId: 'https://comment.weekdaycare.cn',
-                    pageSize: 15,
+                    envId: api,
+                    pageSize: limit,
                     includeReply: false
                 }).then(function (res) {
                     for (var j = 0; j < res.length; j++) {
-                        if (j === 0) {
-                            console.log(res[0]);
-                        }
                         var cell = '<div class="timenode" index="' + i + '">';
                         cell += '<div class="header">';
                         cell += '<div class="user-info">';
-                        cell += '<img src="' + (res[j].avatar || default_avatar) + '" onerror="javascript:this.src=\'' + default_avatar + '\';">';
                         cell += '<span>' + res[j].nick + '</span>';
                         cell += '</div>';
                         cell += '<span>' + res[j].relativeTime + '</span>';
