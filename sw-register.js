15602c79495c7859a0cece98762e001e169a20bd
weekdaycare <xiabee@duck.com>
2024-03-28 00:36:48 +0800

    🎉Update my site

--- sw-register.js
+++ sw-register.js
@@ -1,8 +1,8 @@
-navigator.serviceWorker && navigator.serviceWorker.register('/sw.js?v=20240328002505').then(function() {
+navigator.serviceWorker && navigator.serviceWorker.register('/sw.js?v=20240328003609').then(function() {
     navigator.serviceWorker.addEventListener('message', function(event) {
         if ('sw.update' === event.data) {
             // 使用hud.toast函数显示更新通知
-            hud.toast('已更新最新版本（刷新生效）', 2500);
+            hud.toast('已更新最新版本（刷新生效）', 4000);
 
             // 可选：如果您希望在显示通知后自动刷新页面，可以在这里添加逻辑
             // setTimeout(() => location.reload(), 2500);
