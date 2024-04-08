b3c463b8d9093c2283b369f0ff523117b62d317e
weekdaycare <xiabee@duck.com>
2024-04-08 13:18:58 +0800

    🎉Update my site

--- sw-register.js
+++ sw-register.js
@@ -1,1 +1,1 @@
-navigator.serviceWorker && navigator.serviceWorker.register('/sw.js?v=20240407213351').then(function() {navigator.serviceWorker.addEventListener('message', function(event) {if ('sw.update' === event.data) {hud.toast('已更新，刷新生效', 5000);}});});
+navigator.serviceWorker && navigator.serviceWorker.register('/sw.js?v=20240408131809').then(function() {navigator.serviceWorker.addEventListener('message', function(event) {if ('sw.update' === event.data) {hud.toast('已更新，刷新生效', 5000);}});});
