f6c8c142402062824ce1035abae85c043553a29a
weekdaycare <xiabee@duck.com>
2024-10-10 13:28:27 +0800

    🎉Update my site

--- js/services/webinfo.js
+++ js/services/webinfo.js
@@ -3,9 +3,16 @@ utils.jq(() => {
 	  const els = document.getElementsByClassName('ds-webinfo');
 	  for (var i = 0; i < els.length; i++) {
 		const el = els[i];
-		const time = el.getAttribute('data-time');
-		diffDate = util.diffDate(time, false);
-		$(el).prepend(diffDate);
+		const time = el.getAttribute('datetime');
+		const version = el.hasAttribute('version');
+		if (time) {
+			const diffDate = util.diffDate(time, false);
+			$(el).prepend(diffDate);
+		}
+		if (version) {
+			const versionInfo = localStorage.getItem('lastVersion') || 'NULL';
+			$(el).append(versionInfo);
+		}
 	  }
 	});
   });
