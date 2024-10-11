c2a68abd13df5284d8957671f23a0389556c2972
weekdaycare <xiabee@duck.com>
2024-10-11 16:16:23 +0800

    🎉Update my site

--- js/services/coding.js
+++ js/services/coding.js
@@ -1,18 +1,25 @@
 utils.jq(() => {
   const els = document.getElementsByClassName('ds-coding');
-  for (var i = 0; i < els.length; i++) {
+  let cssLoaded = false;
+  for (let i = 0; i < els.length; i++) {
     const el = els[i];
-    const src = el.getAttribute('url');
-    const lang = el.getAttribute('lang');
-    const css = el.getAttribute('withcss') === 'true';
-    if (lang !== null) {
-      src += '&lang=' + lang
-    }
-    utils.request(el, src, function(data) {
-      $(el).append(data.result);
-    });
-    if (css) {
-      utils.css('https://jsd.hzchu.top/gh/thun888/asstes@master/files/pygments-css/default.css')
-    }
+    const loadContent = () => {
+      const srcBase = el.getAttribute('url');
+      const lang = el.getAttribute('lang');
+      const css = el.getAttribute('withcss') === 'true';
+      let src = srcBase;
+      if (lang !== null) {
+        src += '&lang=' + lang;
+      }
+      if (css && !cssLoaded) {
+        utils.css('https://jsd.hzchu.top/gh/thun888/asstes@master/files/pygments-css/default.css');
+        cssLoaded = true;
+      }
+      utils.request(el, src, function(data) {
+        $(el).append(data.result);
+      });
+    };
+    const lazyload = el.hasAttribute('lazyload');
+    util.viewportLazyload(el, loadContent, lazyload);
   }
 });
