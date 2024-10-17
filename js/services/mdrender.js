85bfc650b4a623514a972a9dfd2bf191ae66a124
weekdaycare <xiabee@duck.com>
2024-10-17 15:15:18 +0800

    🎉Update my site

--- js/services/mdrender.js
+++ js/services/mdrender.js
@@ -1,16 +1,1 @@
-utils.jq(() => {
-  $(function () {
-    const els = document.getElementsByClassName('ds-mdrender');
-    for (var i = 0; i < els.length; i++) {
-      const el = els[i];
-      const loadMarkdown = () => {
-        const src = `${el.getAttribute('src')}?t=${new Date().getTime()}`;
-        utils.request(el, src, function(data) {
-          el.innerHTML = marked.parse(resp.data);
-        });
-      };
-      const lazyload = el.hasAttribute('lazyload');
-      util.viewportLazyload(el, loadMarkdown, lazyload);
-    }
-  });
-});
+utils.jq((()=>{$((function(){const t=document.getElementsByClassName("ds-mdrender");for(var e=0;e<t.length;e++){const n=t[e],s=()=>{const t=`${n.getAttribute("src")}?t=${(new Date).getTime()}`;utils.request(n,t,(function(t){n.innerHTML=marked.parse(resp.data)}))},a=n.hasAttribute("lazyload");util.viewportLazyload(n,s,a)}}))}));
--- js/services/webinfo.js
+++ js/services/webinfo.js
@@ -1,18 +1,1 @@
-utils.jq(() => {
-  $(function () {
-    const els = document.getElementsByClassName('ds-webinfo');
-    for (var i = 0; i < els.length; i++) {
-      const el = els[i];
-      const time = el.getAttribute('datetime');
-      const version = el.hasAttribute('version');
-      if (time) {
-        const diffDate = util.diffDate(time, false);
-        $(el).prepend(diffDate);
-      }
-      if (version) {
-        const versionInfo = localStorage.getItem('lastVersion') || 'NULL';
-        $(el).append(versionInfo);
-      }
-    }
-  });
-});
+utils.jq((()=>{$((function(){const t=document.getElementsByClassName("ds-webinfo");for(var e=0;e<t.length;e++){const n=t[e],s=n.getAttribute("datetime"),i=n.hasAttribute("version");if(s){const t=util.diffDate(s,!1);$(n).prepend(t)}if(i){const t=localStorage.getItem("lastVersion")||"NULL";$(n).append(t)}}}))}));
